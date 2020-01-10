59cf6bca9c2564a7a90afca75990c8cac055185f
Дамир Муртазин <d.m.murtazin@mail.ru>
2020-01-10 20:49:12 +0300

    damirmurtazin/uslugi

---
+++ try11/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace try11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.db1DataSet = new try11.db1DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.paidBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.paidzBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.paid_zTableAdapter = new try11.db1DataSetTableAdapters.paid_zTableAdapter();
+            this.paidTableAdapter = new try11.db1DataSetTableAdapters.paidTableAdapter();
+            this.servicesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.servicesTableAdapter = new try11.db1DataSetTableAdapters.servicesTableAdapter();
+            this.фамилияDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.имяDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.классDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.наименованиеDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.заМесяцDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.суммаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.датаОплатыDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.db1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paidBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paidzBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.servicesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(627, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 31);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 31);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Изменить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.servicesBindingSource;
+            this.comboBox1.DisplayMember = "Наименование";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // db1DataSet
+            // 
+            this.db1DataSet.DataSetName = "db1DataSet";
+            this.db1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.фамилияDataGridViewTextBoxColumn,
+            this.имяDataGridViewTextBoxColumn,
+            this.классDataGridViewTextBoxColumn,
+            this.наименованиеDataGridViewTextBoxColumn,
+            this.заМесяцDataGridViewTextBoxColumn,
+            this.суммаDataGridViewTextBoxColumn,
+            this.датаОплатыDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.paidzBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // paidBindingSource
+            // 
+            this.paidBindingSource.DataMember = "paid";
+            this.paidBindingSource.DataSource = this.db1DataSet;
+            // 
+            // paidzBindingSource
+            // 
+            this.paidzBindingSource.DataMember = "paid_z";
+            this.paidzBindingSource.DataSource = this.db1DataSet;
+            // 
+            // paid_zTableAdapter
+            // 
+            this.paid_zTableAdapter.ClearBeforeFill = true;
+            // 
+            // paidTableAdapter
+            // 
+            this.paidTableAdapter.ClearBeforeFill = true;
+            // 
+            // servicesBindingSource
+            // 
+            this.servicesBindingSource.DataMember = "services";
+            this.servicesBindingSource.DataSource = this.db1DataSet;
+            // 
+            // servicesTableAdapter
+            // 
+            this.servicesTableAdapter.ClearBeforeFill = true;
+            // 
+            // фамилияDataGridViewTextBoxColumn
+            // 
+            this.фамилияDataGridViewTextBoxColumn.DataPropertyName = "Фамилия";
+            this.фамилияDataGridViewTextBoxColumn.HeaderText = "Фамилия";
+            this.фамилияDataGridViewTextBoxColumn.Name = "фамилияDataGridViewTextBoxColumn";
+            // 
+            // имяDataGridViewTextBoxColumn
+            // 
+            this.имяDataGridViewTextBoxColumn.DataPropertyName = "Имя";
+            this.имяDataGridViewTextBoxColumn.HeaderText = "Имя";
+            this.имяDataGridViewTextBoxColumn.Name = "имяDataGridViewTextBoxColumn";
+            // 
+            // классDataGridViewTextBoxColumn
+            // 
+            this.классDataGridViewTextBoxColumn.DataPropertyName = "Класс";
+            this.классDataGridViewTextBoxColumn.HeaderText = "Класс";
+            this.классDataGridViewTextBoxColumn.Name = "классDataGridViewTextBoxColumn";
+            // 
+            // наименованиеDataGridViewTextBoxColumn
+            // 
+            this.наименованиеDataGridViewTextBoxColumn.DataPropertyName = "Наименование";
+            this.наименованиеDataGridViewTextBoxColumn.HeaderText = "Наименование";
+            this.наименованиеDataGridViewTextBoxColumn.Name = "наименованиеDataGridViewTextBoxColumn";
+            // 
+            // заМесяцDataGridViewTextBoxColumn
+            // 
+            this.заМесяцDataGridViewTextBoxColumn.DataPropertyName = "за месяц";
+            this.заМесяцDataGridViewTextBoxColumn.HeaderText = "за месяц";
+            this.заМесяцDataGridViewTextBoxColumn.Name = "заМесяцDataGridViewTextBoxColumn";
+            // 
+            // суммаDataGridViewTextBoxColumn
+            // 
+            this.суммаDataGridViewTextBoxColumn.DataPropertyName = "Сумма";
+            this.суммаDataGridViewTextBoxColumn.HeaderText = "Сумма";
+            this.суммаDataGridViewTextBoxColumn.Name = "суммаDataGridViewTextBoxColumn";
+            // 
+            // датаОплатыDataGridViewTextBoxColumn
+            // 
+            this.датаОплатыDataGridViewTextBoxColumn.DataPropertyName = "Дата оплаты";
+            this.датаОплатыDataGridViewTextBoxColumn.HeaderText = "Дата оплаты";
+            this.датаОплатыDataGridViewTextBoxColumn.Name = "датаОплатыDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 268);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.db1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paidBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paidzBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.servicesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private db1DataSet db1DataSet;
+        private System.Windows.Forms.BindingSource paidzBindingSource;
+        private db1DataSetTableAdapters.paid_zTableAdapter paid_zTableAdapter;
+        private System.Windows.Forms.BindingSource paidBindingSource;
+        private db1DataSetTableAdapters.paidTableAdapter paidTableAdapter;
+        private System.Windows.Forms.BindingSource servicesBindingSource;
+        private db1DataSetTableAdapters.servicesTableAdapter servicesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn фамилияDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn имяDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn классDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn наименованиеDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn заМесяцDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn суммаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn датаОплатыDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ try11/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace try11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "db1DataSet.services". При необходимости она может быть перемещена или удалена.
+            this.servicesTableAdapter.Fill(this.db1DataSet.services);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "db1DataSet.paid". При необходимости она может быть перемещена или удалена.
+            this.paidTableAdapter.Fill(this.db1DataSet.paid);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "db1DataSet.paid_z". При необходимости она может быть перемещена или удалена.
+            this.paid_zTableAdapter.Fill(this.db1DataSet.paid_z);
+
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            paidzBindingSource.Filter = comboBox1.DisplayMember + " = " + comboBox1.Text.ToString();
+        }
+    }
+}
